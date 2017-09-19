cfdf531c1b60c89f2cb2ce17691a17d60d3a5316
Steven Difelippo <matrixdriver04@gmail.com>
2017-09-19 17:42:07 -0400

    merged data into one file

---
+++ src/data/data.js
@@ -0,0 +1,79 @@
+module.exports = {
+        "rare": {
+          id: 1,
+          name: "Rare Chest Armor",
+          price: "100 Glimmer",
+          image: require('../images/rareChest.jpg'),
+          imageAlt: "Rare Chest Armor",
+          details: "A rare chest piece."
+        },
+          {
+          id: 2,
+          name: "Rare Boots",
+          price: "100 Glimmer",
+          image: require('../images/rareLegs.jpg'),
+          imageAlt: "Rare Leg Armor",
+          details: "Rare leg armor"
+        },
+        {
+          id: 3,
+          name: "Rare Gauntlets",
+          price: "100 Glimmer",
+          image: require('../images/rareGauntlets.png'),
+          imageAlt: "Rare Gauntlets",
+          details: "These are rare Gauntlets"
+        }],
+
+        "legendary": [
+          {
+            id: 1,
+            name: "Legendry Chest Armor",
+            price: "500 Glimmer",
+            image: require('../images/legendChest.png'),
+            imageAlt: "Legendry Chest Armor",
+            details: "This legendary chest armor will increase the amount of damage you can take."
+          },
+            {
+            id: 2,
+            name: "Legendary Boots",
+            price: "500 Glimmer",
+            image: require('../images/legendBoots.png'),
+            imageAlt: "Legendary Boots",
+            details: "These legendary boots will increase your sprint speed."
+          },
+          {
+            id: 3,
+            name: "Legendary Gauntlets",
+            price: "500 Glimmer",
+            image: require('../images/legendGauntlets.jpg'),
+            imageAlt: "Legendary Gauntlets",
+            details: "These legendary Gauntlets will increase the reload speed of primary weapons."
+          },
+        ],
+        
+        "exotic": [
+          {
+            id: 1,
+            name: "Nezarec's Sin",
+            price: "1000 Glimmer",
+            image: require('../images/exoticHelm.jpg'),
+            imageAlt: "Exotic helmet",
+            details: "Kills will increase ability recharge rate"
+          },
+            {
+            id: 2,
+            name: "Wings Of Sacred Dawn",
+            price: "1000 Glimmer",
+            image: require('../images/exoticChest.jpg'),
+            imageAlt: "Exotic chest Armor",
+            details: "This chest piece will suspend you in midair while shooting and precision damage in increased"
+          },
+          {
+            id: 3,
+            name: "Doom Fang Pauldron",
+            price: "1000 Glimmer",
+            image: require('../images/exoticGauntlets.jpg'),
+            imageAlt: "Exotic Gauntlets",
+            details: "Shield Bash melee kills recharge Shield Throw. Melee ability kills recharge Sentinel Shield Super."
+          },
+        ];
